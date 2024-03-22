204016e4b921c05671f4985b6a9ab7852ff038f5
hlmg <devhlmg@gmail.com>
2024-03-22 18:51:01 +0900

    fix: cors 설정 수정

--- src/main/java/com/powerGoorm/WebConfig.java
+++ src/main/java/com/powerGoorm/WebConfig.java
@@ -35,7 +35,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("Authorization", "Content-Type");
             }
